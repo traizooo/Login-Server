a2119727de9f263d364c5dcb3ae30a395dfcdfee
Michael <michal.owsiejew23@gmail.com>
2023-03-28 00:12:18 +0100

    Commit before heroku push

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-console.log(process.env.MONGODB_URI);
+console.log(process.env.MONGODB_URL);
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
