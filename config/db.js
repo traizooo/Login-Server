7145ade772c8b1d5a02b0cb4c3d29eda59289c75
Michael <michal.owsiejew23@gmail.com>
2023-03-28 00:25:58 +0100

    Fix: bcrypt error fixed.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-console.log(process.env.MONGODB_URL);
+console.log(process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
